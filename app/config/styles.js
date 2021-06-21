f8b4c566c650373a86ff732c62183dc33189fa13
jessicajourneycake <40467945+jessicajourneycake@users.noreply.github.com>
2021-06-21 17:10:46 -0400

    Add files via upload

---
+++ app/config/styles.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Platform} from 'react-native';
+
+export default {
+    text: {
+        color: "black",
+        fontSize: 18,
+        fontFamily: "Avenir",
+    }
+}
